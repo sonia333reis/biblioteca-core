e3c524dc5b3d0cc59761801128cd2ed0eaa4c70f
Sonia Reis <sonia333reis@gmail.com>
2020-02-11 23:51:46 -0300

    C3 - Instalação de Pacotes e Início da Criação de Funções CRUD

---
+++ biblioteca/Repositories/adoCrudUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using biblioteca.Models;
+using MySql.Data.MySqlClient;
+
+namespace biblioteca.Repositories
+{
+    public class adoCrudUser
+    {
+        private MySqlConnection _con;
+
+        private void Connection() 
+        {
+            _con = new MySqlConnection("DefaultConnection");
+        }
+    }
+}
