b7e55e2752b50a59a4c6a3b966e7dbc13a4a2af9
Sonia Reis <sonia333reis@gmail.com>
2020-02-12 22:38:51 -0300

    C4 - Criação das Funções de CRUD com ADO

--- biblioteca/Repositories/adoCrudUser.cs
+++ biblioteca/Repositories/adoCrudUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using biblioteca.Models;
@@ -14,6 +15,93 @@ namespace biblioteca.Repositories
         private void Connection() 
         {
             _con = new MySqlConnection("DefaultConnection");
+        }
+
+        public bool createUser(User user) 
+        {
+            Connection();
+
+            int i;
+
+            using (MySqlCommand command = new MySqlCommand("createUser",_con)) 
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@uName", user.Nome);
+                command.Parameters.AddWithValue("@uCpf", user.Cpf);
+                command.Parameters.AddWithValue("@uEmail", user.Email);
+                _con.Open();
+                i = command.ExecuteNonQuery();
+            }
+            _con.Close();
+
+            return i >= 1;
+        }
+
+        public List<User> selectAllUsers() 
+        {
+            Connection();
+            List<User> users = new List<User>();
+
+            using (MySqlCommand command = new MySqlCommand("selectAllUsers", _con))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                _con.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read()) 
+                {
+                    User user = new User()
+                    {
+                        Nome = Convert.ToString(reader["UserName"]),
+                        Cpf = Convert.ToString(reader["UserCpf"]),
+                        Email = Convert.ToString(reader["UserEmail"])
+                    };
+
+                    users.Add(user);
+                }
+            }
+            _con.Close();
+
+            return users;
+        }
+
+        public bool updateUser(User user)
+        {
+            Connection();
+
+            int i;
+
+            using (MySqlCommand command = new MySqlCommand("updateUser", _con))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@uName", user.Nome);
+                command.Parameters.AddWithValue("@uCpf", user.Cpf);
+                command.Parameters.AddWithValue("@uEmail", user.Email);
+                command.Parameters.AddWithValue("@uId", user.UserID);
+                _con.Open();
+                i = command.ExecuteNonQuery();
+            }
+            _con.Close();
+
+            return i >= 1;
+        }
+
+        public bool deleteUser(int id)
+        {
+            Connection();
+
+            int i;
+
+            using (MySqlCommand command = new MySqlCommand("deleteUser", _con))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@uId",id);
+                _con.Open();
+                i = command.ExecuteNonQuery();
+            }
+            _con.Close();
+
+            return i >= 1;
         }
     }
 }
